07f58364de6b21c7d5ab8a5b2cbeedab4566c3cb
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-06-28 22:58:58 +0900

    2

--- Natural.java
+++ Natural.java
@@ -1,9 +1,18 @@
 package hanul;
 
+import java.util.Scanner;
+
 public class Natural {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		if(N>1 && N<100000){
+			for(int i=N;i>0;i--){
+			System.out.println(i);
+			}
+		}
+		
 
 	}
 
