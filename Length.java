5ff504d44c0973f690f070f25362b85790bdfaad
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-06-29 17:04:33 +0900

    4

---
+++ Length.java
@@ -0,0 +1,15 @@
+package hanul;
+
+import java.util.Scanner;
+
+public class Length {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String A = in.next();
+		System.out.println(A.length());
+		
+
+	}
+
+}
--- Pibonaci.java
+++ Pibonaci.java
@@ -1,9 +1,23 @@
 package hanul;
 
+import java.util.Scanner;
+
 public class Pibonaci {
 
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int a = 0;
+		int b = 1;
+		int tmp=0;
+		for(int i=0;i<n;i++){
+			tmp= a+b;
+			b=a;
+			a=tmp;
+			
+			
+		}
+		System.out.println(tmp);
 
 	}
 
