bcf840c7de1d74e242b24678cba1e6ec3a691822
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-07-21 15:54:28 +0900

    invariable.java

--- Invariable.java
+++ Invariable.java
@@ -3,33 +3,31 @@ package hanul;
 import java.util.Scanner;
 
 public class Invariable {
-
-		public static void main(String[] args) {
-			Scanner in= new Scanner(System.in);
-			char arr1[] = new char[100];
-			char arr2[] = new char[100];
-			String A = in.next();
-			String B = in.next();
-			
-			if(A!=B){
-			for(int i=0;i<A.length();i++){
-				arr1[i] = A.charAt(A.length()-i-1);
-			}
-			for(int j=0;j<B.length();j++){
-				arr2[j] = B.charAt(B.length()-j-1);
-			}
-			for(int z=0; z<A.length(); z++){
-				if((int)arr1[0] != (int)arr2[0]){
-					if((int)arr1[z] < (int)arr2[z]){
-						System.out.println(arr2);
-						return;
-					}
-					else{
-						System.out.println(arr1);
-					}
-					
-				}
-			}
-		}
+	public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        char arr1[] = new char[3];
+        char arr2[] = new char[3];
+        String A = in.next();
+        String B = in.next();
+         
+        for(int i=0; i<A.length();i++)
+            arr1[i] = A.charAt(A.length()-i-1);
+        
+        for(int j=0; j<B.length();j++)
+            arr2[j] = B.charAt(B.length()-j-1);
+        
+        
+        String r1 = String.valueOf(arr1);
+        String r2 = String.valueOf(arr2);
+        
+        int num1 = Integer.valueOf(r1);
+        int num2 = Integer.valueOf(r2);
+        
+        if(num1 > num2)
+        	System.out.println(num1);
+        else
+        	System.out.println(num2);
+        
 	}
+
 }
