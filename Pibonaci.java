3f26ce3967d99d7c9d0245f174dd4e2a3ec7dfdc
DaEunKim <ekdms717@naver.com>
2016-08-09 15:43:54 +0900

    Pibonaci.java

--- Pibonaci.java
+++ Pibonaci.java
@@ -7,7 +7,6 @@ public class Pibonaci {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		int a = 0;
 		int a = 0; 
 		int b = 1;
 		int tmp=0;
